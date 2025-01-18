60f377293c57a44c6ade7d0d6067a5284a6013d5
Pedro Borges <p260628@dac.unicamp.br>
2025-01-18 15:28:03 -0300

    inicialização do banco de dados

--- trackJobs/main.py
+++ trackJobs/main.py
@@ -1,6 +1,7 @@
 from rich.console import Console
 from rich.prompt import IntPrompt
 from rich.panel import Panel
+import sqlite3
 
 CADASTRAR_CANDIDATURA = 1
 EDITAR_STATUS = 2 
@@ -9,8 +10,38 @@ REMOVER_CANDIDATURA = 4
 
 console = Console()
 
+def inicializa_banco():
+        conexao = sqlite3.connect("track_jobs.db")
+        cursor = conexao.cursor()
+
+        cursor.execute(
+            """create table if not exists empresas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            site TEXT,
+            setor TEXT
+            )"""
+        )
+
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS vagas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            link TEXT NOT NULL,
+            data_aplicacao DATE,
+            status TEXT DEFAULT 'em análise',
+            detalhes TEXT,
+            idEmpresa INTEGER,
+            FOREIGN KEY(idEmpresa) REFERENCES empresas(id)
+            )"""
+            )
+
+        conexao.close()
+
 def main():
     console.print(Panel("[bold magenta]TrackJobs - Gerenciador de Candidaturas[/bold magenta]", expand=False))
+
+    inicializa_banco()
 
     msg_prompt = (
         "[bold cyan]Escolha uma opção abaixo:[/bold cyan]\n\n"

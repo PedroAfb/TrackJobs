332b3c603af19e63a4163e6ce30faac432014bb5
Pedro Borges <p260628@dac.unicamp.br>
2025-01-18 16:41:49 -0300

    fazendo o cadastro de vagas e empresas

--- trackJobs/main.py
+++ trackJobs/main.py
@@ -1,12 +1,15 @@
 from rich.console import Console
-from rich.prompt import IntPrompt
+from rich.prompt import IntPrompt, Prompt
 from rich.panel import Panel
+import datetime
+import click
 import sqlite3
 
 CADASTRAR_CANDIDATURA = 1
 EDITAR_STATUS = 2 
 EDITAR_CANDIDATURA = 3
 REMOVER_CANDIDATURA = 4
+DATA_NULA = '0/0/0'
 
 console = Console()
 
@@ -17,7 +20,7 @@ def inicializa_banco():
         cursor.execute(
             """create table if not exists empresas(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nome TEXT NOT NULL,
+            nome TEXT NOT NULL UNIQUE,
             site TEXT,
             setor TEXT
             )"""
@@ -38,6 +41,35 @@ def inicializa_banco():
 
         conexao.close()
 
+def cadastra_candidatura():
+    nome = click.prompt("Qual o nome da vaga?[OBRIGATÓRIO]")
+    link = click.prompt("Qual o link da vaga?[OBRIGATÓRIO]")
+    data = Prompt.ask("Qual foi a data de aplicação?[OPCIONAL]")
+    status = Prompt.ask(
+        "Qual o status da candidatura?[OPCIONAL]",
+        choices=["em análise", "entrevista", "rejeitado", "aceito"],
+        default="em análise",
+        show_default=False)
+    detalhes = Prompt.ask("Coloque detalhes sobre a vaga[OPCIONAL]")
+    nome_empresa = Prompt.ask("Qual o nome da empresa?[OPCIONAL]")
+
+    conexao = sqlite3.connect("track_jobs.db")
+    cursor = conexao.cursor()
+
+    lista_empresas = cursor.execute("""select nome from empresas""").fetchall()
+    print(lista_empresas)
+
+    if nome_empresa and nome_empresa not in lista_empresas:
+        site_empresa = Prompt.ask("Qual o site da empresa?[OPCIONAL]")
+        setor_empresa = Prompt.ask("Qual o setor da empresa?[OPCIONAL]")
+    
+        cursor.execute(f"""
+            INSERT INTO empresas (nome, site, setor) VALUES
+            ('{nome_empresa}', '{site_empresa}', '{setor_empresa}')
+            """)
+        conexao.commit()
+
+
 def main():
     console.print(Panel("[bold magenta]TrackJobs - Gerenciador de Candidaturas[/bold magenta]", expand=False))
 
@@ -50,13 +82,13 @@ def main():
         "[green]3[/green] - Editar Candidatura\n"
         "[green]4[/green] - Remover Candidatura\n"
     )
-    code = IntPrompt.ask(msg_prompt, choices=['1', '2', '3', '4'])
+    opcao = IntPrompt.ask(msg_prompt, choices=['1', '2', '3', '4'])
 
-    if code == CADASTRAR_CANDIDATURA:
+    if opcao == CADASTRAR_CANDIDATURA:
+        cadastra_candidatura()
+    elif opcao == EDITAR_STATUS:
         pass
-    elif code == EDITAR_STATUS:
-        pass
-    elif code == EDITAR_CANDIDATURA:
+    elif opcao == EDITAR_CANDIDATURA:
         pass
     else:
         pass

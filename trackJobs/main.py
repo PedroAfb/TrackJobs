3cb3aa83b01a17186bad7c0185a3016b73bcbf50
Pedro Borges <p260628@dac.unicamp.br>
2025-01-18 14:36:26 -0300

    estilizando o prompt

--- trackJobs/main.py
+++ trackJobs/main.py
@@ -1,14 +1,24 @@
+from rich.console import Console
 from rich.prompt import IntPrompt
+from rich.panel import Panel
 
 CADASTRAR_CANDIDATURA = 1
 EDITAR_STATUS = 2 
 EDITAR_CANDIDATURA = 3
 REMOVER_CANDIDATURA = 4
+
+console = Console()
+
 def main():
-    msg_prompt = "1 - Cadastrar Candidatura\n"
-    msg_prompt += "2 - Editar status da candidatura\n"
-    msg_prompt += "3 - Editar candidatura\n"
-    msg_prompt += "4 - Remover candidatura"
+    console.print(Panel("[bold magenta]TrackJobs - Gerenciador de Candidaturas[/bold magenta]", expand=False))
+
+    msg_prompt = (
+        "[bold cyan]Escolha uma opção abaixo:[/bold cyan]\n\n"
+        "[green]1[/green] - Cadastrar Candidatura\n"
+        "[green]2[/green] - Editar Status da Candidatura\n"
+        "[green]3[/green] - Editar Candidatura\n"
+        "[green]4[/green] - Remover Candidatura\n"
+    )
     code = IntPrompt.ask(msg_prompt, choices=['1', '2', '3', '4'])
 
     if code == CADASTRAR_CANDIDATURA:

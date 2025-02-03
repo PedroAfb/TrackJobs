a6d99f3bb33d83d7e676f2956b797954bf061d59
Pedro Borges <p260628@dac.unicamp.br>
2025-02-03 18:29:06 -0300

    refactor(cadastro): refatorando funcionalidade

--- trackJobs/cadastro.py
+++ trackJobs/cadastro.py
@@ -1,9 +1,12 @@
-import validators
-from rich.prompt import Prompt
-from rich.console import Console
-import click
 import sqlite3
-from sqlite3 import Connection, Cursor
+from sqlite3 import Connection
+from sqlite3 import Cursor
+
+import click
+import validators
+from rich.console import Console
+from rich.prompt import Prompt
+
 from .exceptions import RetornarMenuException
 
 console = Console()
@@ -20,15 +23,23 @@ def obter_site_empresa(cursor: Cursor):
             cursor.execute("SELECT 1 FROM empresas WHERE site = ?", (site_empresa,))
             if not cursor.fetchone():
                 return site_empresa
-            console.print("[bold red]Essa URL já foi cadastrada. Digite um link válido ou deixe em branco.[/bold red]")
+            msg = (
+                "[bold red]Essa URL já foi cadastrada.[/bold red]"
+                "[bold red] Digite um link válido ou deixe em branco.[/bold red]"
+            )
+            console.print(msg)
 
         else:
-            console.print("[bold red]URL inválida. Digite um link válido ou deixe em branco.[/bold red]")
+            msg = (
+                "[bold red]URL inválida. [/bold red]"
+                "[bold red]Digite um link válido ou deixe em branco.[/bold red]"
+            )
+            console.print(msg)
 
 
 def obter_link_vaga(db_path="track_jobs.db"):
     link = None
-    while link != '6':
+    while link != "6":
         link = Prompt.ask("Qual o link da vaga?[OBRIGATÓRIO]")
 
         if validators.url(link):
@@ -38,8 +49,11 @@ def obter_link_vaga(db_path="track_jobs.db"):
 
             if not cursor.fetchone():
                 return link
-            
-            console.print("[bold red]Essa URL já foi cadastrada. Digite um link válido.[/bold red]")
+            msg = (
+                "[bold red]Essa URL já foi cadastrada. [/bold red]"
+                "[bold red]Digite um link válido.[/bold red]"
+            )
+            console.print(msg)
             console.print("Caso queira retornar ao menu principal, digite 6")
 
             conexao.close()
@@ -59,8 +73,8 @@ def coleta_dados_vaga():
     dados_candidatura = dict()
 
     nome = click.prompt(
-        "Qual o nome da vaga?[OBRIGATÓRIO]\n"
-        "Caso queira retornar ao menu, digite 6")
+        "Qual o nome da vaga?[OBRIGATÓRIO]\n" "Caso queira retornar ao menu, digite 6"
+    )
     dados_candidatura["nome"] = nome.strip().lower()
 
     dados_candidatura["link"] = obter_link_vaga()
@@ -68,37 +82,57 @@ def coleta_dados_vaga():
         "Qual o status da candidatura?[OPCIONAL]",
         choices=["candidatar-se", "em análise", "entrevista", "rejeitado", "aceito"],
         default="candidatar-se",
-        show_default=False)
-    dados_candidatura["descricao"] = Prompt.ask("Coloque descrição sobre a vaga[OPCIONAL]")
+        show_default=False,
+    )
+    dados_candidatura["descricao"] = Prompt.ask(
+        "Coloque descrição sobre a vaga[OPCIONAL]"
+    )
     nome_empresa = Prompt.ask("Qual o nome da empresa?[OPCIONAL]")
     dados_candidatura["nome_empresa"] = nome_empresa.strip().lower()
 
     return dados_candidatura
 
 
-def cadastra_empresa(conexao: Connection, cursor: Cursor, nome_empresa: str):
+def coleta_dados_empresa(cursor: Cursor):
     site_empresa = obter_site_empresa(cursor)
     setor_empresa = Prompt.ask("Qual o setor da empresa?[OPCIONAL]")
+    return site_empresa, setor_empresa
 
-    msg_insert_empresas = (f"""
+
+def cadastra_empresa(conexao: Connection, cursor: Cursor, nome_empresa: str):
+    site_empresa, setor_empresa = coleta_dados_empresa(cursor)
+
+    msg_insert_empresas = f"""
         INSERT INTO empresas (nome, site, setor) VALUES
         ('{nome_empresa}', '{site_empresa}', '{setor_empresa}')
-        """)
+        """
     cursor.execute(msg_insert_empresas)
     conexao.commit()
 
+    console.print(
+        "[bold green]\nCadastro da empresa realizado com sucesso!\n[/bold green]"
+    )
+
 
 def cadastra_vaga(conexao: Connection, cursor: Cursor, dados_candidatura: dict):
-    if dados_candidatura["nome_empresa"]:    # Adiciona vaga com uma empresa associada
-        id_empresa = cursor.execute("SELECT id FROM empresas WHERE nome = ?", (dados_candidatura["nome_empresa"],)).fetchall()[0][0]
+    if dados_candidatura["nome_empresa"]:  # Adiciona vaga com uma empresa associada
+        id_empresa = cursor.execute(
+            "SELECT id FROM empresas WHERE nome = ?",
+            (dados_candidatura["nome_empresa"],),
+        ).fetchall()[0][0]
         msg_insert_vagas = (
-            "INSERT INTO vagas (nome, link, status, descricao, idEmpresa) VALUES\n"
-            f"('{dados_candidatura['nome']}', '{dados_candidatura['link']}', '{dados_candidatura['status']}', '{dados_candidatura['descricao']}', '{id_empresa}')"
+            "INSERT INTO vagas "
+            "(nome, link, status, descricao, idEmpresa) VALUES\n"
+            f"('{dados_candidatura['nome']}', '{dados_candidatura['link']}', "
+            f"'{dados_candidatura['status']}', "
+            f"'{dados_candidatura['descricao']}', '{id_empresa}')"
         )
     else:
         msg_insert_vagas = (
-        "INSERT INTO vagas (nome, link, status, descricao) VALUES\n"
-        f"('{dados_candidatura['nome']}', '{dados_candidatura['link']}', '{dados_candidatura['status']}', '{dados_candidatura['descricao']}')"
+            "INSERT INTO vagas "
+            "(nome, link, status, descricao) VALUES\n"
+            f"('{dados_candidatura['nome']}', '{dados_candidatura['link']}', "
+            "'{dados_candidatura['status']}', '{dados_candidatura['descricao']}')"
         )
 
     cursor.execute(msg_insert_vagas)
@@ -116,17 +150,18 @@ def cadastra_candidatura(db_path="track_jobs.db", teste=False):
 
         nome_empresa = dados_candidatura["nome_empresa"]
         empresa_existe = verifica_empresa_sql(cursor, nome_empresa)
-        
 
-        if nome_empresa and not empresa_existe:    # Se o usuário escreveu no campo empresa e ela não está no banco de dados, o cadastro da empresa é realizado
+        if nome_empresa and not empresa_existe:
             cadastra_empresa(conexao, cursor, nome_empresa)
 
         cadastra_vaga(conexao, cursor, dados_candidatura)
-        
-        console.print("[bold green]\nCadastro realizado com sucesso!\n[/bold green]")
+
+        console.print(
+            "[bold green]\nCadastro da vaga realizado com sucesso!\n[/bold green]"
+        )
         conexao.close()
 
-    except RetornarMenuException as e:
+    except RetornarMenuException:
         pass
 
     except sqlite3.IntegrityError as e:
@@ -134,9 +169,11 @@ def cadastra_candidatura(db_path="track_jobs.db", teste=False):
         erro_duplicado = str(e)
         console.print(f"[bold yellow]Detalhes:[/bold yellow] {erro_duplicado}")
         campo_duplicado = erro_duplicado.split(" ")[-1]
-        console.print(
-            f"[bold red]Erro ao cadastrar no banco de dados: {campo_duplicado} já foi cadastrada![/bold red]"
+        msg = (
+            "[bold red]Erro ao cadastrar no banco de dados: [/bold red]"
+            f"[bold red]{campo_duplicado} já foi cadastrada![/bold red]"
         )
+        console.print(msg)
         if teste:
             raise e
         cadastra_candidatura()

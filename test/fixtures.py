0252891b9f659e82b69cc491049d046a53c3a1b0
Pedro Borges <p260628@dac.unicamp.br>
2025-02-03 18:19:56 -0300

    test(cadastro): adicionando testes de cadastro com empresas

--- test/fixtures.py
+++ test/fixtures.py
@@ -1,10 +1,9 @@
-
 dados_cadastro_sucesso_sem_empresa = {
     "nome": "vaga teste",
     "link": "https://example.com",
     "status": "candidatar-se",
     "descricao": "descrição teste",
-    "nome_empresa": None
+    "nome_empresa": None,
 }
 
 dados_cadastro_sem_empresa_link_duplicado = {
@@ -12,7 +11,7 @@ dados_cadastro_sem_empresa_link_duplicado = {
     "link": "https://example.com",
     "status": "em análise",
     "descricao": "descrição teste",
-    "nome_empresa": None
+    "nome_empresa": None,
 }
 
 dados_cadastro_sem_empresa_link_invalido = {
@@ -20,5 +19,21 @@ dados_cadastro_sem_empresa_link_invalido = {
     "link": "//example.com",
     "status": "candidatar-se",
     "descricao": "descrição teste",
-    "nome_empresa": None
+    "nome_empresa": None,
+}
+
+dados_cadastro_com_empresa = {
+    "nome": "vaga teste",
+    "link": "https://example.com",
+    "status": "candidatar-se",
+    "descricao": "descrição teste",
+    "nome_empresa": "Amazon",
+}
+
+dados_cadastro_com_empresa2 = {
+    "nome": "vaga teste",
+    "link": "https://example23.com",
+    "status": "candidatar-se",
+    "descricao": "descrição teste",
+    "nome_empresa": "Amazon",
 }

72057688eb328d7d95fa71827c5fdb91dc8951b8
Pedro Borges <p260628@dac.unicamp.br>
2025-02-03 18:19:56 -0300

    test(cadastro): adicionando testes de cadastro com empresas

--- test/conftest.py
+++ test/conftest.py
@@ -1,14 +1,32 @@
-from .fixtures import dados_cadastro_sucesso_sem_empresa, dados_cadastro_sem_empresa_link_duplicado, dados_cadastro_sem_empresa_link_invalido
 import pytest
+
+from .fixtures import dados_cadastro_com_empresa
+from .fixtures import dados_cadastro_com_empresa2
+from .fixtures import dados_cadastro_sem_empresa_link_duplicado
+from .fixtures import dados_cadastro_sem_empresa_link_invalido
+from .fixtures import dados_cadastro_sucesso_sem_empresa
+
 
 @pytest.fixture
 def dict_cadastro_sucesso_sem_empresa():
     return dados_cadastro_sucesso_sem_empresa
 
+
 @pytest.fixture
 def dict_cadastro_sem_empresa_link_duplicado():
     return dados_cadastro_sem_empresa_link_duplicado
 
+
 @pytest.fixture
 def dict_cadastro_sem_empresa_link_invalido():
     return dados_cadastro_sem_empresa_link_invalido
+
+
+@pytest.fixture
+def dict_cadastro_com_empresa():
+    return dados_cadastro_com_empresa
+
+
+@pytest.fixture
+def dict_cadastro_com_empresa2():
+    return dados_cadastro_com_empresa2
--- test/test_cadastro.py
+++ test/test_cadastro.py
@@ -1,59 +1,150 @@
-from .db_test import criar_banco_teste, remove_banco_teste
-from unittest.mock import patch
-from trackJobs.exceptions import RetornarMenuException
-from trackJobs.cadastro import cadastra_candidatura, obter_link_vaga
-import pytest
 import sqlite3
+from unittest.mock import patch
+
+import pytest
+
+from .db_test import criar_banco_teste
+from .db_test import remove_banco_teste
+from trackJobs.cadastro import cadastra_candidatura
+from trackJobs.cadastro import obter_link_vaga
+from trackJobs.cadastro import obter_site_empresa
+from trackJobs.exceptions import RetornarMenuException
+
 
 # Testes sem o cadastro de empresa
-def test_cadastro_vaga_sem_empresa(dict_cadastro_sucesso_sem_empresa, capsys):
-    with patch("trackJobs.cadastro.coleta_dados_vaga", return_value=dict_cadastro_sucesso_sem_empresa):
-        criar_banco_teste()
-
+def realiza_cadastro_sem_empresa_com_sucesso(dados_candidatura):
+    with patch("trackJobs.cadastro.coleta_dados_vaga", return_value=dados_candidatura):
         cadastra_candidatura(db_path="track_jobs_test.db")
-        captured_stdout = capsys.readouterr()
-        assert "Cadastro realizado com sucesso!" in captured_stdout.out
+
+
+def test_cadastro_vaga_sem_empresa(dict_cadastro_sucesso_sem_empresa, capsys):
+    criar_banco_teste()
+    realiza_cadastro_sem_empresa_com_sucesso(dict_cadastro_sucesso_sem_empresa)
+    captured_stdout = capsys.readouterr()
+    assert "Cadastro da vaga realizado com sucesso!" in captured_stdout.out
 
     remove_banco_teste()
 
+
 def test_cadastro_vaga_sem_empresa_link_duplicado(
-    dict_cadastro_sucesso_sem_empresa,
-    dict_cadastro_sem_empresa_link_duplicado,
-    capsys):
-    with patch("trackJobs.cadastro.coleta_dados_vaga", return_value=dict_cadastro_sucesso_sem_empresa):
-        criar_banco_teste()
+    dict_cadastro_sucesso_sem_empresa, dict_cadastro_sem_empresa_link_duplicado, capsys
+):
+    criar_banco_teste()
+    realiza_cadastro_sem_empresa_com_sucesso(dict_cadastro_sucesso_sem_empresa)
+    captured_stdout = capsys.readouterr()
+    assert "Cadastro da vaga realizado com sucesso!" in captured_stdout.out
 
-        cadastra_candidatura(db_path="track_jobs_test.db")
-        captured_stdout = capsys.readouterr()
-        assert "Cadastro realizado com sucesso!" in captured_stdout.out
-
-    with patch("trackJobs.cadastro.coleta_dados_vaga", return_value=dict_cadastro_sem_empresa_link_duplicado):
+    with patch(
+        "trackJobs.cadastro.coleta_dados_vaga",
+        return_value=dict_cadastro_sem_empresa_link_duplicado,
+    ):
         with pytest.raises(sqlite3.IntegrityError):
             cadastra_candidatura(db_path="track_jobs_test.db", teste=True)
         captured_stdout = capsys.readouterr()
-        assert "Erro ao cadastrar no banco de dados: vagas.link já foi cadastrada!" in captured_stdout.out
+        assert (
+            "Erro ao cadastrar no banco de dados: vagas.link já foi cadastrada!"
+            in captured_stdout.out
+        )
 
     remove_banco_teste()
 
-@pytest.mark.parametrize("mock_input, expected_output", [
-    (["invalid_url", "https://vaga.com/456"], "https://vaga.com/456"),  # Primeira inválida, depois válida
-    (["6"], RetornarMenuException),  # Usuário deseja sair
-])
+
+@pytest.mark.parametrize(
+    "mock_input, expected_output",
+    [
+        (
+            ["invalid_url", "https://vaga.com/456"],
+            "https://vaga.com/456",
+        ),  # Primeira inválida, depois válida
+        (["6"], RetornarMenuException),  # Usuário deseja sair
+    ],
+)
 def test_obter_link_vaga(mock_input, expected_output):
     criar_banco_teste()
     with patch("trackJobs.cadastro.Prompt.ask", side_effect=mock_input):
         if expected_output == RetornarMenuException:
-            with pytest.raises(RetornarMenuException):  # Verifica se a exceção foi levantada
+            with pytest.raises(
+                RetornarMenuException
+            ):  # Verifica se a exceção foi levantada
                 obter_link_vaga(db_path="track_jobs_test.db")
         else:
-            assert obter_link_vaga(db_path="track_jobs_test.db") == expected_output  # Verifica se retornou o link correto
+            assert (
+                obter_link_vaga(db_path="track_jobs_test.db") == expected_output
+            )  # Verifica se retornou o link correto
     remove_banco_teste()
 
 
-#Testes com o cadastro de empresa
-'''
-cadastro com sucesso
-nome duplicado
-link duplicado
-link com formato inválido
-'''
+# Testes com o cadastro de empresa
+def realiza_cadastro_com_empresa(dados_candidatura):
+    with patch(
+        "trackJobs.cadastro.coleta_dados_vaga", return_value=dados_candidatura
+    ), patch(
+        "trackJobs.cadastro.coleta_dados_empresa",
+        return_value=("https://example.com", "Tech"),
+    ) as mock_dados_empresa:
+        cadastra_candidatura(db_path="track_jobs_test.db")
+
+    return mock_dados_empresa
+
+
+def test_cadastro_vaga_com_empresa_n_cadastrada(dict_cadastro_com_empresa, capsys):
+    criar_banco_teste()
+    realiza_cadastro_com_empresa(dict_cadastro_com_empresa)
+    captured_stdout = capsys.readouterr()
+    assert "Cadastro da empresa realizado com sucesso" in captured_stdout.out
+    assert "Cadastro da vaga realizado com sucesso!" in captured_stdout.out
+
+    remove_banco_teste()
+
+
+def test_cadastro_vaga_com_empresa_cadastrada(
+    dict_cadastro_com_empresa, dict_cadastro_com_empresa2, capsys
+):
+    criar_banco_teste()
+    realiza_cadastro_com_empresa(dict_cadastro_com_empresa)
+    captured_stdout = capsys.readouterr()
+    assert "Cadastro da empresa realizado com sucesso" in captured_stdout.out
+    assert "Cadastro da vaga realizado com sucesso!" in captured_stdout.out
+
+    mock_dados_empresa = realiza_cadastro_com_empresa(dict_cadastro_com_empresa2)
+
+    captured_stdout = capsys.readouterr()
+    assert "Cadastro da vaga realizado com sucesso!" in captured_stdout.out
+    mock_dados_empresa.assert_not_called()
+
+    remove_banco_teste()
+
+
+def test_site_invalido_empresa(capsys):
+    criar_banco_teste()
+    prompt_input = ["invalid_url", "https://vaga.com/456"]
+    expected_output = "URL inválida. Digite um link válido ou deixe em branco."
+
+    with patch("trackJobs.cadastro.Prompt.ask", side_effect=prompt_input):
+        conexao = sqlite3.connect("track_jobs_test.db")
+        cursor = conexao.cursor()
+
+        obter_site_empresa(cursor)
+        captured_stdout = capsys.readouterr()
+        assert expected_output in captured_stdout.out
+
+    remove_banco_teste()
+
+
+def test_site_duplicado_empresa(dict_cadastro_com_empresa, capsys):
+    criar_banco_teste()
+    prompt_input = ["https://example.com", "https://example23.com"]
+    expected_output = (
+        "Essa URL já foi cadastrada. Digite um link válido ou deixe em branco."
+    )
+
+    realiza_cadastro_com_empresa(dict_cadastro_com_empresa)
+    with patch("trackJobs.cadastro.Prompt.ask", side_effect=prompt_input):
+        conexao = sqlite3.connect("track_jobs_test.db")
+        cursor = conexao.cursor()
+
+        obter_site_empresa(cursor)
+        captured_stdout = capsys.readouterr()
+        assert expected_output in captured_stdout.out
+
+    remove_banco_teste()

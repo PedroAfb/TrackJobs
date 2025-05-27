7ebd675982203a034ec05c0361f92d6936790843
Pedro Borges <p260628@dac.unicamp.br>
2025-05-27 16:14:43 -0300

    refactor(exceptions): adicionando exceções mais específicas

--- trackJobs/exceptions.py
+++ trackJobs/exceptions.py
@@ -1,6 +1,53 @@
-class InicializacaoBancoException(Exception):
-    pass
+class TrackJobsException(Exception):
+    def __init__(self, message="Erro desconhecido no TrackJobs."):
+        super().__init__(message)
 
 
-class RetornarMenuException(Exception):
-    pass
+class InicializacaoBancoException(TrackJobsException):
+    def __init__(self, message="Erro ao inicializar o banco de dados."):
+        super().__init__(message)
+
+
+class RetornarMenuException(TrackJobsException):
+    def __init__(self, message="Retornando ao menu principal."):
+        super().__init__(message)
+
+
+class URLJaCadastradaException(TrackJobsException):
+    def __init__(self, message="Erro: URL já cadastrada."):
+        super().__init__(message)
+
+
+class URLNaoInformadaException(TrackJobsException):
+    def __init__(self, message="Erro: URL não informada."):
+        super().__init__(message)
+
+
+class NomeVazioException(TrackJobsException):
+    def __init__(self, message="Erro: O nome não pode ser vazio."):
+        super().__init__(message)
+
+
+class URLInvalidaException(TrackJobsException):
+    def __init__(self, message="Erro: Formato da URL inválida."):
+        super().__init__(message)
+
+
+class DataInvalidaException(TrackJobsException):
+    def __init__(self, message="Erro: Formato da Data é inválido."):
+        super().__init__(message)
+
+
+class StatusInvalidoException(TrackJobsException):
+    def __init__(self, message="Erro: Status inválido."):
+        super().__init__(message)
+
+
+class ErroCandidaturaException(TrackJobsException):
+    def __init__(self, message="Erro: ao realizar candidatura."):
+        super().__init__(message)
+
+
+class CampoDuplicadoException(TrackJobsException):
+    def __init__(self, message="Erro: Esse Campo já foi cadastrado."):
+        super().__init__(message)

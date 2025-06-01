8efaa4563e71a71530097ec886dec9a9f8bb3349
Pedro Borges <p260628@dac.unicamp.br>
2025-06-01 12:36:18 -0300

    refactor(empresa-repository): implementação explícita da interface e injeção de independência
    Além disso, todos os selects agora retornam o respectivo id da empresa

--- trackJobs/model/repositories/SQLite/sqlite_empresa_repository.py
+++ trackJobs/model/repositories/SQLite/sqlite_empresa_repository.py
@@ -1,22 +1,27 @@
 from typing import Optional
 
 from trackJobs.model.entities.empresa import Empresa
+from trackJobs.model.repositories.interfaces.empresa_repository import EmpresaRepository
 from trackJobs.model.repositories.SQLite.base_repository import BaseSQLiteRepository
 
 
-class SQLiteEmpresaRepository(BaseSQLiteRepository):
+class SQLiteEmpresaRepository(EmpresaRepository):
+    def __init__(self, db):
+        self.db = db
+        self.base_repository = BaseSQLiteRepository(db)
+
     def listar_campos_empresa(self) -> list[str]:
         """Retorna os campos necessários para cadastro de empresa"""
-        with self.transaction() as cursor:
+        with self.base_repository.transaction() as cursor:
             cursor.execute("PRAGMA table_info(empresas)")
             colunas_empresas = [row[1] + "_empresa" for row in cursor.fetchall()]
 
         colunas_empresas.remove("id_empresa")
         return colunas_empresas
 
-    def cadastrar_empresa(self, empresa: Empresa) -> None:
-        """Cadastra uma nova empresa no banco de dados"""
-        with self.transaction() as cursor:
+    def cadastrar_empresa(self, empresa: Empresa) -> int:
+        """Cadastra uma nova empresa no banco de dados e retorna o id"""
+        with self.base_repository.transaction() as cursor:
             msg_insert_empresas = """
                 INSERT INTO empresas (nome, site, setor) VALUES
             (?, ?, ?)
@@ -29,9 +34,10 @@ class SQLiteEmpresaRepository(BaseSQLiteRepository):
                     empresa.setor if empresa.setor else None,
                 ),
             )
+            return cursor.lastrowid
 
     def listar_nome_empresas(self) -> list[str]:
-        with self.transaction() as cursor:
+        with self.base_repository.transaction() as cursor:
             cursor.execute("SELECT nome FROM empresas ORDER BY nome")
             return [row[0].capitalize() for row in cursor.fetchall()]
 
@@ -40,26 +46,34 @@ class SQLiteEmpresaRepository(BaseSQLiteRepository):
         if not nome:
             return None
 
-        with self.transaction() as cursor:
+        with self.base_repository.transaction() as cursor:
             cursor.execute(
-                "SELECT nome, site, setor FROM empresas WHERE nome = ?", (nome,)
+                """SELECT id, nome, site, setor
+                FROM empresas WHERE nome = ?""",
+                (nome.lower(),),
             )
             empresa = cursor.fetchone()
 
         if empresa:
-            return Empresa(nome=empresa[0], site=empresa[1], setor=empresa[2])
+            return Empresa(
+                id=empresa[0], nome=empresa[1], site=empresa[2], setor=empresa[3]
+            )
         return None
 
     def buscar_empresa_por_link(self, link: str) -> Optional[Empresa]:
         if not link:
             return None
 
-        with self.transaction() as cursor:
+        with self.base_repository.transaction() as cursor:
             cursor.execute(
-                "SELECT nome, site, setor FROM empresas WHERE site = ?", (link,)
+                """SELECT id, nome, site, setor
+                FROM empresas WHERE site = ?""",
+                (link,),
             )
             empresa = cursor.fetchone()
 
         if empresa:
-            return Empresa(nome=empresa[0], site=empresa[1], setor=empresa[2])
+            return Empresa(
+                id=empresa[0], nome=empresa[1], site=empresa[2], setor=empresa[3]
+            )
         return None

ab685a51cbc08cb1ac3401a13acd7125b698bdcf
Pedro Borges <p260628@dac.unicamp.br>
2025-06-01 12:39:25 -0300

    refactor(job-model): novo código para a refatoração usando repositores, entities e services

--- trackJobs/model/job_model.py
+++ trackJobs/model/job_model.py
@@ -1,44 +1,56 @@
-from .cadastro import cadastra_candidatura
-from .cadastro import get_campos_cadastro_empresa
-from .cadastro import get_campos_cadastro_vaga
-from .cadastro import get_empresa_por_nome
-from .validador import VALIDADORES
+from .repositories.SQLite.sqlite_empresa_repository import SQLiteEmpresaRepository
+from .repositories.SQLite.sqlite_vaga_repository import SQLiteVagaRepository
 from trackJobs.banco_de_dados import BancoDeDados
 from trackJobs.exceptions import TrackJobsException
+from trackJobs.model.entities.vaga import Vaga
+from trackJobs.model.services.candidatura_service import CandidaturaService
+from trackJobs.model.services.validadores.empresa_validador_service import (
+    EmpresaValidadorService,
+)
+from trackJobs.model.services.validadores.vaga_validador_service import (
+    VagaValidadorService,
+)
+from trackJobs.model.services.validadores.validador_service import ValidadorService
 
 
 class JobModel:
     def __init__(self, db_path: str = "track_jobs.db"):
         self.db = BancoDeDados(db_path)
 
-    def cadastro(self, dados_candidatura):
+    def cadastro(self, dados_candidatura: Vaga):
         """Cadastra uma nova candidatura"""
-        cadastra_candidatura(dados_candidatura)
+        empresa_repository = SQLiteEmpresaRepository(self.db)
+        vaga_repository = SQLiteVagaRepository(self.db)
+        candidatura_service = CandidaturaService(empresa_repository, vaga_repository)
 
-    def validar_campo(self, campo, valor, console):
+        try:
+            candidatura_service.cadastra_candidatura(dados_candidatura)
+        except TrackJobsException as e:
+            raise e
+
+    def validar_campo(self, campo, valor):
         """Valida o campo de acordo com os validadores definidos"""
-        if campo in VALIDADORES:
-            try:
-                return VALIDADORES[campo](self.db, valor, console)
-            except TrackJobsException as e:
-                raise e
-        return False
+        empresa_validador = EmpresaValidadorService(SQLiteEmpresaRepository(self.db))
+        vaga_validador = VagaValidadorService(SQLiteVagaRepository(self.db))
+        validador_service = ValidadorService(empresa_validador, vaga_validador)
+        return validador_service.VALIDADORES[campo](valor)
 
     def campos_cadastro_vaga(self):
         """Retorna os campos necessários para cadastro de vaga"""
-        return get_campos_cadastro_vaga(self.db)
+        vaga_repository = SQLiteVagaRepository(self.db)
+        return vaga_repository.listar_campos_vaga()
 
     def campos_cadastro_empresa(self):
         """Retorna os campos necessários para cadastro de empresa"""
-        return get_campos_cadastro_empresa(self.db)
+        empresa_repository = SQLiteEmpresaRepository(self.db)
+        return empresa_repository.listar_campos_empresa()
 
     def listar_nome_empresas(self):
         """Retorna lista de empresas cadastradas"""
-        cursor = self.db.cursor
-        cursor.execute("SELECT nome FROM empresas ORDER BY nome")
-        empresas = [row[0].capitalize() for row in cursor.fetchall()]
-        return empresas
+        empresa_repository = SQLiteEmpresaRepository(self.db)
+        return empresa_repository.listar_nome_empresas()
 
     def get_empresa(self, nome_empresa):
         """Retorna os dados de uma empresa através do nome"""
-        return get_empresa_por_nome(self.db, nome_empresa)
+        empresa_repository = SQLiteEmpresaRepository(self.db)
+        return empresa_repository.buscar_empresa_por_nome(nome_empresa)

f1e5a4362ca280c74a7f83924ef97a4ac0cce5fe
Pedro Borges <p260628@dac.unicamp.br>
2025-06-05 19:10:32 -0300

    refactor(candidatura-services): regra de negocio para a funcionalidade de update

--- trackJobs/model/services/candidatura_service.py
+++ trackJobs/model/services/candidatura_service.py
@@ -1,3 +1,4 @@
+from trackJobs.exceptions import CampoInvalidoException
 from trackJobs.model.entities.vaga import Vaga
 from trackJobs.model.repositories.interfaces.empresa_repository import EmpresaRepository
 from trackJobs.model.repositories.interfaces.vaga_repository import VagaRepository
@@ -19,3 +20,19 @@ class CandidaturaService:
 
         vaga.empresa = empresa
         self.vaga_repository.cadastrar_candidatura(vaga)
+
+    def filtra_vagas(self, filtro: str = "", tipo_filtro: str = "") -> list[Vaga]:
+        """Filtra vagas com base no nome ou descrição"""
+        return self.vaga_repository.get_vaga_com_filtro(filtro, tipo_filtro)
+
+    def get_vaga_por_link(self, link: str) -> Vaga:
+        """Busca uma vaga pelo link"""
+        return self.vaga_repository.buscar_vaga_por_link(link)
+
+    def atualiza_vaga(self, vaga: Vaga, campo_update: str, novo_dado: str):
+        """Atualiza uma vaga existente"""
+        if campo_update not in self.vaga_repository.listar_campos_vaga():
+            raise CampoInvalidoException(
+                f"Campo '{campo_update}' não é válido para atualização."
+            )
+        self.vaga_repository.atualizar_vaga(vaga, campo_update, novo_dado)

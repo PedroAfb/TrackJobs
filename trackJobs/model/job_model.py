abb9582e94f90a3c1922c3beb1d7d04ddd5e4322
Pedro Borges <p260628@dac.unicamp.br>
2025-06-05 19:11:43 -0300

    refactor(job-model): métodos para a funcionalidade de edicao na interface do model

--- trackJobs/model/job_model.py
+++ trackJobs/model/job_model.py
@@ -16,15 +16,16 @@ from trackJobs.model.services.validadores.validador_service import ValidadorServ
 class JobModel:
     def __init__(self, db_path: str = "track_jobs.db"):
         self.db = BancoDeDados(db_path)
+        empresa_repository = SQLiteEmpresaRepository(self.db)
+        vaga_repository = SQLiteVagaRepository(self.db)
+        self.candidatura_service = CandidaturaService(
+            empresa_repository, vaga_repository
+        )
 
     def cadastro(self, dados_candidatura: Vaga):
         """Cadastra uma nova candidatura"""
-        empresa_repository = SQLiteEmpresaRepository(self.db)
-        vaga_repository = SQLiteVagaRepository(self.db)
-        candidatura_service = CandidaturaService(empresa_repository, vaga_repository)
-
         try:
-            candidatura_service.cadastra_candidatura(dados_candidatura)
+            self.candidatura_service.cadastra_candidatura(dados_candidatura)
         except TrackJobsException as e:
             raise e
 
@@ -54,3 +55,15 @@ class JobModel:
         """Retorna os dados de uma empresa através do nome"""
         empresa_repository = SQLiteEmpresaRepository(self.db)
         return empresa_repository.buscar_empresa_por_nome(nome_empresa)
+
+    def get_vaga_por_link(self, link: str):
+        """Busca uma vaga pelo link"""
+        return self.candidatura_service.get_vaga_por_link(link)
+
+    def candidaturas_filtradas(self, filtro: str = "", tipo_filtro: str = ""):
+        """Filtra candidaturas com base no nome ou descrição"""
+        return self.candidatura_service.filtra_vagas(filtro, tipo_filtro)
+
+    def atualizar_vaga(self, vaga: Vaga, campo_update: str, novo_dado: str):
+        """Atualiza uma vaga existente"""
+        return self.candidatura_service.atualiza_vaga(vaga, campo_update, novo_dado)

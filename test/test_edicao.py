ac20f8596101397a7da9ddde01f768eb4dcf0db3
Pedro Borges <p260628@dac.unicamp.br>
2025-06-05 16:56:23 -0300

    style(trackjobs): deixando algumas funcionalidades que não estão no estilo mvc inativas

--- test/test_edicao.py
+++ test/test_edicao.py
@@ -1,77 +1,77 @@
-from unittest.mock import patch
+# from unittest.mock import patch
 
-import pytest
+# import pytest
 
-from .db_test import criar_banco_teste_com_dados
-from .db_test import remove_banco_teste
-from .test_menu import setup_tela_mock
-from .test_menu import verifica_saida_esperada
-from trackJobs.banco_de_dados import BancoDeDados
-from trackJobs.edicao import edicao
-from trackJobs.edicao import MenuEdicao
-from trackJobs.menu import Menu
-
-
-def get_vaga_id(db_path, id):
-    db = BancoDeDados(db_path)
-    cursor = db.cursor
-    cursor.execute(f"SELECT * FROM vagas WHERE id = {id}")
-    vaga = cursor.fetchone()
-    if vaga:
-        # Pega o nome das colunas
-        colunas = [descricao[0] for descricao in cursor.description]
-
-        # Cria um dicionário combinando as colunas com os valores
-        vaga_dict = dict(zip(colunas, vaga))
-
-        return vaga_dict
-    else:
-        return None  # Caso não encontre a vaga
+# from .db_test import criar_banco_teste_com_dados
+# from .db_test import remove_banco_teste
+# from .test_menu import setup_tela_mock
+# from .test_menu import verifica_saida_esperada
+# from trackJobs.banco_de_dados import BancoDeDados
+# from trackJobs.edicao import edicao
+# from trackJobs.edicao import MenuEdicao
+# from trackJobs.menu import Menu
 
 
-@pytest.mark.parametrize(
-    "campo_alterado, novo_valor, msg_esperada",
-    [
-        ("nome", "Dev Fullstack JR", "esperado_msg_edicao"),
-        (
-            "link",
-            "https://jobs.example.com/25",
-            "esperado_msg_link",
-        ),
-        (
-            "data_aplicaçao",
-            "2021-10-10",
-            "esperado_msg_data",
-        ),
-        (
-            "status",
-            "rejeitado",
-            "esperado_msg_status",
-        ),
-        (
-            "descriçao",
-            "Desenvolver novas features",
-            "esperado_msg_descricao",
-        ),
-    ],
-)
-@patch.object(Menu, "menu_candidaturas", return_value=2)
-def test_edicao(mock_menu, campo_alterado, novo_valor, msg_esperada, request):
-    criar_banco_teste_com_dados()
-    id_da_vaga_escolhida = "3"
-    msg_esperada = request.getfixturevalue(msg_esperada)
+# def get_vaga_id(db_path, id):
+#     db = BancoDeDados(db_path)
+#     cursor = db.cursor
+#     cursor.execute(f"SELECT * FROM vagas WHERE id = {id}")
+#     vaga = cursor.fetchone()
+#     if vaga:
+#         # Pega o nome das colunas
+#         colunas = [descricao[0] for descricao in cursor.description]
 
-    with patch.object(MenuEdicao, "menu_edicao", return_value=f"{campo_alterado}"):
-        with patch("questionary.text") as mock_text:
-            mock_text.return_value.ask.return_value = novo_valor
-            with open("saida_teste.txt", "w") as saida:
-                with patch("curses.curs_set"):
-                    tela_mock = setup_tela_mock(saida)
-                    edicao(tela_mock, db_path="track_jobs_test.db")
+#         # Cria um dicionário combinando as colunas com os valores
+#         vaga_dict = dict(zip(colunas, vaga))
 
-    verifica_saida_esperada(msg_esperada)
+#         return vaga_dict
+#     else:
+#         return None  # Caso não encontre a vaga
 
-    candidatura_alterada = get_vaga_id("track_jobs_test.db", id_da_vaga_escolhida)
-    assert candidatura_alterada[f"{campo_alterado}"] == novo_valor
 
-    remove_banco_teste()
+# @pytest.mark.parametrize(
+#     "campo_alterado, novo_valor, msg_esperada",
+#     [
+#         ("nome", "Dev Fullstack JR", "esperado_msg_edicao"),
+#         (
+#             "link",
+#             "https://jobs.example.com/25",
+#             "esperado_msg_link",
+#         ),
+#         (
+#             "data_aplicaçao",
+#             "2021-10-10",
+#             "esperado_msg_data",
+#         ),
+#         (
+#             "status",
+#             "rejeitado",
+#             "esperado_msg_status",
+#         ),
+#         (
+#             "descriçao",
+#             "Desenvolver novas features",
+#             "esperado_msg_descricao",
+#         ),
+#     ],
+# )
+# @patch.object(Menu, "menu_candidaturas", return_value=2)
+# def test_edicao(mock_menu, campo_alterado, novo_valor, msg_esperada, request):
+#     criar_banco_teste_com_dados()
+#     id_da_vaga_escolhida = "3"
+#     msg_esperada = request.getfixturevalue(msg_esperada)
+
+#     with patch.object(MenuEdicao, "menu_edicao", return_value=f"{campo_alterado}"):
+#         with patch("questionary.text") as mock_text:
+#             mock_text.return_value.ask.return_value = novo_valor
+#             with open("saida_teste.txt", "w") as saida:
+#                 with patch("curses.curs_set"):
+#                     tela_mock = setup_tela_mock(saida)
+#                     edicao(tela_mock, db_path="track_jobs_test.db")
+
+#     verifica_saida_esperada(msg_esperada)
+
+#     candidatura_alterada = get_vaga_id("track_jobs_test.db", id_da_vaga_escolhida)
+#     assert candidatura_alterada[f"{campo_alterado}"] == novo_valor
+
+#     remove_banco_teste()
--- trackJobs/edicao.py
+++ trackJobs/edicao.py
@@ -1,148 +1,148 @@
-import curses
+# import curses
 
-import questionary
-from rich.console import Console
+# import questionary
+# from rich.console import Console
 
-from .exceptions import RetornarMenuException
-from .menu import CANDIDATURA_SELECIONADA
-from .menu import Menu
-from .menu import MOVER_BAIXO
-from .menu import MOVER_CIMA
-from .menu import VOLTAR_MENU
-from .utils import get_vaga
-from .utils import realiza_update
-from .validador import VALIDADORES
+# from .exceptions import RetornarMenuException
+# from .menu import CANDIDATURA_SELECIONADA
+# from .menu import Menu
+# from .menu import MOVER_BAIXO
+# from .menu import MOVER_CIMA
+# from .menu import VOLTAR_MENU
+# from .utils import get_vaga
+# from .utils import realiza_update
+# from .validador import VALIDADORES
 
-CAMPOS_VAGA = ["nome", "link", "data_aplicaçao", "status", "descriçao"]
-
-
-class MenuEdicao(Menu):
-    def __init__(self, tela, db_path="track_jobs.db"):
-        super().__init__(tela, db_path=db_path)
-        self.index_campo_atual = 0
-
-    def exibir_campo(self, campo, dados, i, campo_pra_print):
-        _, largura = self.tela.getmaxyx()
-        margem = 2  # Margem de segurança para evitar estouro
-        if campo == "data_aplicaçao":
-            campo = "data de aplicação"
-        elif campo == "descriçao":
-            campo = "descrição"
-
-        if (
-            campo_pra_print == self.index_campo_atual
-        ):  # Se o cursor estiver no campo, mostra o valor
-            msg = f"> {campo.capitalize()}: {dados}\n"
-        else:
-            msg = f"> {campo.capitalize()}\n"
-
-        largura_disponivel = max(
-            0, largura - margem * 2
-        )  # Garante que não fique negativo
-        msg_truncada = msg[
-            : largura_disponivel + 3
-        ]  # Corta a mensagem para caber na tela
-
-        estilo = (
-            curses.A_REVERSE
-            if campo_pra_print == self.index_campo_atual
-            else curses.A_NORMAL
-        )
-        self.tela.addstr(i + 2, 2, msg_truncada, estilo)
-
-    def interpreta_teclado_menu_edicao(self):
-        entrada_user = self.tela.getch()  # Aguarda entrada do teclado
-
-        if (
-            entrada_user == MOVER_BAIXO
-            and self.index_campo_atual < len(CAMPOS_VAGA) - 1
-        ):
-            self.index_campo_atual += 1
-        elif entrada_user == MOVER_CIMA and self.index_campo_atual > 0:
-            self.index_campo_atual -= 1
-        elif entrada_user == CANDIDATURA_SELECIONADA:
-            self.tela.clear()
-            return CAMPOS_VAGA[self.index_campo_atual]
-        elif entrada_user == VOLTAR_MENU:
-            self.tela.clear()
-            raise RetornarMenuException
-
-        return None
-
-    def exibir_menu_edicao(
-        self, candidatura: dict, itens_exibidos: int, posicao_scroll: int
-    ):
-        msg = (
-            "Escolha o campo que queira editar da vaga escolhida abaixo:\n"
-            "(Setas para navegar, Enter para selecionar "
-            "e ESC para retornar ao menu principal)\n\n"
-        )
-        self.tela.addstr(0, 0, msg)
-
-        self.index_candidatura_atual = self.index_campo_atual
-        posicao_scroll = self.ajustar_scroll(posicao_scroll, itens_exibidos)
-
-        for i, campo in enumerate(CAMPOS_VAGA):
-            campo_pra_print = i + posicao_scroll
-            self.exibir_campo(campo, candidatura[campo], i, campo_pra_print)
-
-    def menu_edicao(self, candidatura: dict):
-        curses.curs_set(0)  # Oculta o cursor no terminal
-        self.tela.keypad(True)
-        self.tela.clear()
-
-        self.index_campo_atual = 0
-        posicao_scroll = 0
-
-        while True:
-            max_linhas, _ = self.tela.getmaxyx()
-            itens_exibidos = max_linhas - 5
-
-            self.tela.clear()
-
-            self.exibir_menu_edicao(candidatura, itens_exibidos, posicao_scroll)
-
-            result = self.interpreta_teclado_menu_edicao()
-            if result:
-                return result
-
-    def exibe_mensagem_sucesso(self, novo_status, campo_atualizado="Status"):
-        if campo_atualizado == "Data_aplicaçao":
-            campo_atualizado = "Data de aplicação"
-        self.tela.clear()
-        self.tela.addstr(
-            5, 5, f"✅ Campo {campo_atualizado} atualizado com sucesso!", curses.A_BOLD
-        )
-        self.tela.addstr(7, 5, "Pressione qualquer tecla para voltar ao menu principal")
-        self.tela.getch()  # Espera pressionar uma tecla
+# CAMPOS_VAGA = ["nome", "link", "data_aplicaçao", "status", "descriçao"]
 
 
-def edicao(tela, db_path="track_jobs.db"):
-    try:
-        menu_edicao = MenuEdicao(tela, db_path)
+# class MenuEdicao(Menu):
+#     def __init__(self, tela, db_path="track_jobs.db"):
+#         super().__init__(tela, db_path=db_path)
+#         self.index_campo_atual = 0
 
-        cand_selecionada = menu_edicao.escolha_candidatura()
-        cand_selecionada = get_vaga(menu_edicao.db, cand_selecionada["link"])
-        campo_selecionado = menu_edicao.menu_edicao(cand_selecionada)
-        tela.clear()
-        while True:
-            novo_dado = (
-                questionary.text(
-                    "\nInforme o novo valor do campo "
-                    f"{campo_selecionado.capitalize()}:\n",
-                )
-                .ask()
-                .strip()
-            )
-            if VALIDADORES[campo_selecionado](db_path, novo_dado, Console()):
-                break
+#     def exibir_campo(self, campo, dados, i, campo_pra_print):
+#         _, largura = self.tela.getmaxyx()
+#         margem = 2  # Margem de segurança para evitar estouro
+#         if campo == "data_aplicaçao":
+#             campo = "data de aplicação"
+#         elif campo == "descriçao":
+#             campo = "descrição"
 
-        realiza_update(menu_edicao.db, cand_selecionada, campo_selecionado, novo_dado)
+#         if (
+#             campo_pra_print == self.index_campo_atual
+#         ):  # Se o cursor estiver no campo, mostra o valor
+#             msg = f"> {campo.capitalize()}: {dados}\n"
+#         else:
+#             msg = f"> {campo.capitalize()}\n"
 
-        menu_edicao.exibe_mensagem_sucesso(None, campo_selecionado.capitalize())
+#         largura_disponivel = max(
+#             0, largura - margem * 2
+#         )  # Garante que não fique negativo
+#         msg_truncada = msg[
+#             : largura_disponivel + 3
+#         ]  # Corta a mensagem para caber na tela
 
-    except RetornarMenuException:
-        pass
+#         estilo = (
+#             curses.A_REVERSE
+#             if campo_pra_print == self.index_campo_atual
+#             else curses.A_NORMAL
+#         )
+#         self.tela.addstr(i + 2, 2, msg_truncada, estilo)
 
-    except Exception as e:
-        menu_edicao.exibe_mensagem_erro(e)
+#     def interpreta_teclado_menu_edicao(self):
+#         entrada_user = self.tela.getch()  # Aguarda entrada do teclado
+
+#         if (
+#             entrada_user == MOVER_BAIXO
+#             and self.index_campo_atual < len(CAMPOS_VAGA) - 1
+#         ):
+#             self.index_campo_atual += 1
+#         elif entrada_user == MOVER_CIMA and self.index_campo_atual > 0:
+#             self.index_campo_atual -= 1
+#         elif entrada_user == CANDIDATURA_SELECIONADA:
+#             self.tela.clear()
+#             return CAMPOS_VAGA[self.index_campo_atual]
+#         elif entrada_user == VOLTAR_MENU:
+#             self.tela.clear()
+#             raise RetornarMenuException
+
+#         return None
+
+#     def exibir_menu_edicao(
+#         self, candidatura: dict, itens_exibidos: int, posicao_scroll: int
+#     ):
+#         msg = (
+#             "Escolha o campo que queira editar da vaga escolhida abaixo:\n"
+#             "(Setas para navegar, Enter para selecionar "
+#             "e ESC para retornar ao menu principal)\n\n"
+#         )
+#         self.tela.addstr(0, 0, msg)
+
+#         self.index_candidatura_atual = self.index_campo_atual
+#         posicao_scroll = self.ajustar_scroll(posicao_scroll, itens_exibidos)
+
+#         for i, campo in enumerate(CAMPOS_VAGA):
+#             campo_pra_print = i + posicao_scroll
+#             self.exibir_campo(campo, candidatura[campo], i, campo_pra_print)
+
+#     def menu_edicao(self, candidatura: dict):
+#         curses.curs_set(0)  # Oculta o cursor no terminal
+#         self.tela.keypad(True)
+#         self.tela.clear()
+
+#         self.index_campo_atual = 0
+#         posicao_scroll = 0
+
+#         while True:
+#             max_linhas, _ = self.tela.getmaxyx()
+#             itens_exibidos = max_linhas - 5
+
+#             self.tela.clear()
+
+#             self.exibir_menu_edicao(candidatura, itens_exibidos, posicao_scroll)
+
+#             result = self.interpreta_teclado_menu_edicao()
+#             if result:
+#                 return result
+
+#     def exibe_mensagem_sucesso(self, novo_status, campo_atualizado="Status"):
+#         if campo_atualizado == "Data_aplicaçao":
+#             campo_atualizado = "Data de aplicação"
+#         self.tela.clear()
+#         self.tela.addstr(
+#             5, 5, f"✅ Campo {campo_atualizado} atualizado com sucesso!", curses.A_BOLD
+#         )
+#         self.tela.addstr(7, 5, "Pressione qualquer tecla para voltar ao menu principal")
+#         self.tela.getch()  # Espera pressionar uma tecla
+
+
+# def edicao(tela, db_path="track_jobs.db"):
+#     try:
+#         menu_edicao = MenuEdicao(tela, db_path)
+
+#         cand_selecionada = menu_edicao.escolha_candidatura()
+#         cand_selecionada = get_vaga(menu_edicao.db, cand_selecionada["link"])
+#         campo_selecionado = menu_edicao.menu_edicao(cand_selecionada)
+#         tela.clear()
+#         while True:
+#             novo_dado = (
+#                 questionary.text(
+#                     "\nInforme o novo valor do campo "
+#                     f"{campo_selecionado.capitalize()}:\n",
+#                 )
+#                 .ask()
+#                 .strip()
+#             )
+#             if VALIDADORES[campo_selecionado](db_path, novo_dado, Console()):
+#                 break
+
+#         realiza_update(menu_edicao.db, cand_selecionada, campo_selecionado, novo_dado)
+
+#         menu_edicao.exibe_mensagem_sucesso(None, campo_selecionado.capitalize())
+
+#     except RetornarMenuException:
+#         pass
+
+#     except Exception as e:
+#         menu_edicao.exibe_mensagem_erro(e)

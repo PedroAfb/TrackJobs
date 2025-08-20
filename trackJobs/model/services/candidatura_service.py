0b2e52889c0e8270b1f9dd99b4ab8fcda2d7cd96
Pedro Borges <p260628@dac.unicamp.br>
2025-08-20 18:39:33 -0300

    refactor(services): serviçoes de get refatorados
    pegando as novas mudanças nos repositories e adicionando um serviço a eals

--- trackJobs/model/services/candidatura_service.py
+++ trackJobs/model/services/candidatura_service.py
@@ -1,7 +1,16 @@
 from trackJobs.exceptions import CampoInvalidoException
+from trackJobs.model.entities.empresa import Empresa
+from trackJobs.model.entities.empresa import EmpresaQuery
 from trackJobs.model.entities.vaga import Vaga
+from trackJobs.model.entities.vaga import VagaQuery
 from trackJobs.model.repositories.interfaces.empresa_repository import EmpresaRepository
 from trackJobs.model.repositories.interfaces.vaga_repository import VagaRepository
+from trackJobs.model.services.validadores.empresa_validador_service import (
+    EmpresaValidadorService,
+)
+from trackJobs.model.services.validadores.vaga_validador_service import (
+    VagaValidadorService,
+)
 
 
 class CandidaturaService:
@@ -21,11 +30,20 @@ class CandidaturaService:
                 empresa = self.empresa_repository.cadastrar_empresa(vaga.empresa)
 
         vaga.empresa = empresa
-        self.vaga_repository.cadastrar_candidatura(vaga)
+        return self.vaga_repository.cadastrar_candidatura(vaga)
+
+    def cadastra_empresa(self, empresa: Empresa):
+        """Cadastra uma nova empresa no banco de dados"""
+        empresa = self.empresa_repository.cadastrar_empresa(empresa)
+        return empresa.id
 
     def filtra_vagas(self, filtro: str = "", tipo_filtro: str = "") -> list[Vaga]:
         """Filtra vagas com base no nome, link ou status"""
         return self.vaga_repository.get_vaga_com_filtro(filtro, tipo_filtro)
+
+    def get_vagas(self, filtro: VagaQuery) -> list[Vaga]:
+        """Filtra vagas com base no nome, link ou status"""
+        return self.vaga_repository.get_vaga(filtro)
 
     def get_vaga_por_link(self, link: str) -> Vaga:
         """Busca uma vaga pelo link"""
@@ -42,3 +60,28 @@ class CandidaturaService:
     def remove_vaga(self, vaga: Vaga):
         """Remove uma vaga existente"""
         self.vaga_repository.remover_vaga(vaga)
+
+    def get_empresas(self, filtro: EmpresaQuery) -> list[Empresa]:
+        """Busca empresas com base no filtro fornecido"""
+        return self.empresa_repository.get_empresas(filtro)
+
+    def valida_empresa(self, empresa: Empresa) -> bool:
+        """Valida os dados de uma empresa antes do cadastro"""
+        validador = EmpresaValidadorService(self.empresa_repository)
+        if validador.valida_nome_empresa(
+            empresa.nome
+        ) and validador.valida_link_empresa(empresa.site):
+            return True
+        return False
+
+    def valida_vaga(self, vaga: Vaga) -> bool:
+        """Valida os dados de uma vaga antes do cadastro"""
+        validador = VagaValidadorService(self.vaga_repository)
+        if (
+            validador.valida_nome(vaga.nome)
+            and validador.valida_link(vaga.link)
+            and validador.valida_status(vaga.status)
+            and validador.valida_data_aplicacao(vaga.data_aplicacao)
+        ):
+            return True
+        return False
